e39cfb902d4a9d3bb25cec672b43b0a46602d594
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-15 01:43:04 +0500

    faaf

---
+++ main.js
@@ -0,0 +1,232 @@
+// For 1
+
+// let A = 1;
+// let B = 10;
+
+// for (let i = A; i <= B; i++) {
+//   console.log(i);
+// }
+
+// console.log(B - A + 1);
+
+// For 2
+
+// let A = 1;
+// let B = 10;
+// let s = 0;
+
+// for (let i = B; i > A; i--) {
+//     console.log(i);
+//     s++;
+// }
+
+// console.log(s);
+
+// For 3
+
+// let narx = 5000;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(narx * i);
+// }
+
+// For 4
+
+// let narx = 2000;
+
+// for (let i = 12; i <= 20; i += 2) {
+//     console.log((narx * i) / 10);
+// }
+
+// For 5
+
+// let a = 1;
+// let b = 10;
+// let sum = 0;
+
+// for (i = a; i <= 10; i++) {
+//     console.log(i);
+//     sum += i
+// }
+
+// console.log(sum);
+
+// For 6
+
+// let a = 1;
+// let b = 10;
+// let mul = 1;
+
+// for (let i = a; i <= 10; i++) {
+//   console.log(i);
+//   mul *= i;
+// }
+
+// console.log(mul);
+
+// For 7
+
+// let a = 1;
+// let b = 10;
+// let sum = 0;
+
+// for (let i = a; i <= 10; i++) {
+//   console.log(i);
+//   sum += i ** 2;
+// }
+
+// console.log(sum);
+
+// For 8
+
+// let n = 10;
+// let sum = 0;
+
+// for (i = 1; i <= n; i++) {
+//     sum += 1/i;
+// }
+
+// console.log(sum);
+
+// For 9
+
+// let n = 2;
+// let mul = 1;
+
+// for (let i = 1; i <= n; i+= 0.1) {
+//   mul *= i;
+// }
+
+// console.log(mul);
+
+// For 10
+
+// let n =10;
+// let sum = 0;
+// let cash
+
+// for (let i = 1; i <= n; i++) {
+//   cash = 2*i - 1;
+//   sum = sum + cash;
+//   console.log(i + "^2 = " + i**2 + " => qo'shilma = " + (sum - cash) + " + " + cash);
+// };
+
+// For 11
+
+// let n = 4;
+// let a = 10;
+// let sum = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   sum *= a;
+// }
+// console.log(sum);
+
+// For 12
+
+// let n = 6;
+// let a = 3;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(a ** i);
+// };
+
+// For 13
+
+// let n = 5;
+// let a = 8;
+// let sum = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(a ** i);
+//   sum += a ** i;
+// }
+// console.log("S = " + sum);
+
+// For 14
+
+// let n = 10;
+// let sum = 0;
+// let temp = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   temp *= i;
+//   sum += temp;
+// }
+// console.log("S = " + sum);
+
+// For 15
+
+// let N = 15;
+// let K =10;
+// let sum = 0;
+
+// for (let i = 1; i <= N; i++) {
+//   sum += i ** K;
+// }
+// console.log("S = " + sum);
+
+// For 16
+
+// let N = 5;
+// let sum = 0;
+
+// for (let i = 1; i <= N; i++) {
+//   sum += i ** i;
+// }
+// console.log("S = " + sum);
+
+// For 17
+
+// let A = 4;
+// let B = 10;
+
+// for (let i = A; i <= B; i++) {
+//   for (let j = 0; j <= i-A; j++) {
+//     console.log(i);
+//   }
+// };
+
+// For 18
+
+// let A = 10;
+// let sum = A;
+// let temp = 1;
+
+// for (let i = 1; i <= A / 2; i++) {
+//   if (A % i == 0) {
+//     sum += i;
+//     temp++;
+//     console.log(i + " bo'luvchi");
+//   }
+// }
+
+// console.log(A + "bo'luvchi");
+// console.log(sum + "bo'luvchi yig'indisi");
+// console.log(temp + "bo'luvchilari soni");
+
+// For 19
+
+// let A = 6;
+// let temp = 0;
+
+// for (let i = 1; i <= A / 2; i++) {
+//   if (A % i == 0) {
+//     temp++;
+//   }
+// }
+// if (temp > 1) {
+//   console.log(A + " tub son emas");
+// } else {
+//   console.log(A + " tub son");
+// }
+
+// For 20
+
+// let A = 5;
+// let temp = "";
+
+// for (let i = 1; i <= A; i++) {
+//   temp += i.toFixed()
+//   console.log(temp);
+// };
